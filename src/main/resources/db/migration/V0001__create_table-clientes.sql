6543b886605e595574f1a2080ceb6491af33724e
RegisMichael25 <regis.michael101@gmail.com>
2025-12-13 15:40:03 -0300

    Nova atualização na migration da table clientes

--- src/main/resources/db/migration/V0001__create_table-clientes.sql
+++ src/main/resources/db/migration/V0001__create_table-clientes.sql
@@ -1,7 +1,8 @@
-
 CREATE TABLE clientes (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     endereco VARCHAR(255) NOT NULL,
-    telefone VARCHAR(12)
+    telefone VARCHAR(12),
+    veiculo_id BIGINT,
+    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
 );
